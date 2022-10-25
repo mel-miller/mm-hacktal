e266be9490e0ae192c318a44ca5a0def34d87c5a
Melissa Miller <melissa.miller@pantheon.io>
2022-10-25 17:17:00 -0500

    configure for twig
    Also:
    Start button component
    Remove example component

--- fractal.config.js
+++ fractal.config.js
@@ -2,14 +2,19 @@
 
 const path = require('path');
 const fractal = (module.exports = require('@frctl/fractal').create());
+const twigAdapter = require('@frctl/twig')();
 
+// Global config.
 fractal.set('project.title', 'Hacktal');
-
-fractal.components.set('path', path.join(__dirname, 'components'));
-
-fractal.docs.set('path', path.join(__dirname, 'docs'));
-
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+// Configure components.
+fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.engine(twigAdapter);
+fractal.components.set('ext', '.twig');
+
+// Configure docs.
+fractal.docs.set('path', path.join(__dirname, 'docs'));
 
 // Set build directory.
 fractal.web.set('builder.dest', __dirname + '/build');
