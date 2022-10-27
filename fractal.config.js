487c420c540a07bb128b9ee52d354129870f1dbb
M Miller <melissa.miller@pantheon.io>
2022-10-26 18:59:19 -0700

    update status colors

--- fractal.config.js
+++ fractal.config.js
@@ -55,22 +55,22 @@ fractal.web.theme(mmTheme);
 fractal.components.set('statuses', {
   ready: {
     label: 'Ready',
-    color: '#80c75b',
+    color: '#318604',
     description: 'This component is ready to use in production.',
   },
   beta: {
     label: 'Beta',
-    color: '#54bde9',
+    color: '#1D7F9F',
     description: 'This component is stable, but design could change slightly.',
   },
   alpha: {
     label: 'Alpha',
-    color: '#ff794d',
+    color: '#ba0c2f',
     description: 'Use with caution. This component is still under review and will most likely change.',
   },
   draft: {
     label: 'Draft',
-    color: '#ba0c2f',
+    color: '#666666',
     description: 'Work in progress. Do not use.',
   },
 });
